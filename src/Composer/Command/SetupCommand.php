e005a67576507b7255fe0e120382b51398d82bf3
Philippe Gerber <philippe@bigwhoop.ch>
2011-12-09 02:44:27 +0100

    Started work on setup command

---
+++ src/Composer/Command/SetupCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Philippe Gerber <philippe@bigwhoop.ch>
+ */
+class SetupCommand extends Command
+{
+    protected function configure()
+    {
+        $help = <<<EOT
+The <info>setup</info> command reads the composer.json file from the
+current directory, processes it, and asks your for further application
+configuration/setup.
+
+<info>php composer.phar setup</info>
+EOT;
+
+        $this->setName('setup')
+             ->setDescription('Starts step-by-step configuration of this application')
+             ->setHelp($help);
+    }
+
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $config = $this->getComposer()->getConfig();
+        var_dump($config);
+    }
+}
